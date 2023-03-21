22472cb1baa8b11f13114e215ee61da9bb2cbf8a
Szarvas Péter <peterszarvas94@gmail.com>
2023-03-21 22:51:56 +0100

    add about section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        'reactblue': '#61dafb',
+        'gitred': '#e34f26',
+        'astrored': '#FF5D01',
+      } 
+    },
+
 		fontFamily: {
 			'firasans': ['"Fira Sans"', 'sans'],
       'firamono': ['"Fira Mono"', 'monospace'],
