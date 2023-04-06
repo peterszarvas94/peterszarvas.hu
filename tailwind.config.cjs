7bcab94f335add9f3ceccc6f75f9c6755b5fe0d3
Szarvas Péter <peterszarvas94@gmail.com>
2023-04-06 15:48:42 +0200

    add title animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,54 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-      filter: {
-        'grayscale-80': 'grayscale(80%)',
-      },
+
       gridTemplateColumns: {
         'auto-1fr': 'auto 1fr',
-      }
-    },
-		fontFamily: {
-			'firasans': ['"Fira Sans"', 'sans'],
-      'firamono': ['"Fira Mono"', 'monospace'],
-		}
+      },
+
+		  fontFamily: {
+			  'firasans': ['"Fira Sans"', 'sans'],
+        'firamono': ['"Fira Mono"', 'monospace'],
+	  	},
+
+      keyframes: {
+        fill: {
+          '0%': { 'clip-path': 'inset(0 0 0 0)' },
+          '10%': { 'clip-path': 'inset(0 0 0 100%)' },
+          '15%': { 'clip-path': 'inset(0 0 0 100%)' },
+          '50%': { 'clip-path': 'inset(0 0 0 0)' },
+          '70%': { 'clip-path': 'inset(0 0 0 100%)' },
+          '100%': { 'clip-path': 'inset(0 0 0 0)' },
+        }
+      },
+
+      animation: {
+        fill: 'fill 6s ease infinite',
+      },
+
+    }
 	},
-	plugins: [],
+
+	plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.writing-v': {
+          'writing-mode': 'vertical-rl'
+        },
+        '.grayscale-60': {
+          filter: 'grayscale(60%)'
+        },
+        '.stroke-2-transparent': {
+          '-webkit-text-stroke': '0.1rem transparent'
+        },
+        '.animation-stop': {
+          'animation-play-state': 'paused'
+        },
+      })
+    }),
+  ],
 }
